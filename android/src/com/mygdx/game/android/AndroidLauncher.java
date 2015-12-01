701ff044959790411bbaa952f5e6616bc1d1010b
Mamareza <mamareza.mrn@gmail.com>
2015-12-01 17:00:43 +0330

    Initial commit.

---
+++ android/src/com/mygdx/game/android/AndroidLauncher.java
@@ -0,0 +1,36 @@
+package com.mygdx.game.android;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.badlogic.gdx.backends.android.AndroidFragmentApplication;
+import com.mygdx.game.MyGdxGame;
+
+public class AndroidLauncher extends FragmentActivity implements AndroidFragmentApplication.Callbacks{
+	private static final String TAG = AndroidLauncher.class.getSimpleName();
+	private static final String tag = BlankFragment.class.getSimpleName();
+	private BlankFragment fragment;
+
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		setContentView(R.layout.main);
+		super.onCreate(savedInstanceState);
+		fragment = new BlankFragment();
+		getSupportFragmentManager().beginTransaction().replace(R.id.fragment, fragment, tag).commit();
+	}
+
+	@Override
+	public void exit() {
+		Fragment fragment = getSupportFragmentManager().findFragmentByTag(tag);
+		getSupportFragmentManager().beginTransaction().remove(this.fragment).commit();
+		if (fragment != null){
+			Log.d(TAG, "Fragment removed successfully");
+		}
+		this.fragment = null;
+
+	}
+}
